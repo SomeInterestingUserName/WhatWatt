b22b9081cf2bdd455a90239d9955b3fce5e0c7ce
jchen <jchen2120212@gmail.com>
2022-08-15 21:07:59 -0400

    Initial Commit

---
+++ WhatWatt/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WhatWatt
+//
+//  Created by Jiawei Chen on 8/15/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
